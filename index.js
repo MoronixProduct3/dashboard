2cae8b388e9e9b7d808b3a6b287706e5787da8cf
Édouard Denommée <moronicproduct3@gmail.com>
2019-02-07 22:00:26 -0500

    Moved configs to external file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const puppeteer = require('puppeteer-core');
 
+// Loading settings
+try{
+    var settings = require('./settings.json');
+}catch(e){
+    console.log('Error: Maked sure to create the settings.json file');
+    process.exit(1);
+}
+
 (async () => {
-    const browser = await puppeteer.launch({executablePath:'/usr/bin/chromium'});
+    const browser = await puppeteer.launch({executablePath: settings.chromium_path});
     const page = await browser.newPage();
-    await page.goto('http://securbot.gel.usherbrooke.ca/');
+    await page.goto(settings.url);
     await page.screenshot({path: 'example.png'});
     await browser.close();
 })();
