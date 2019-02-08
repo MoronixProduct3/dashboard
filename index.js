f038ec65461ebf5232ead493d996eec8065ec45d
Édouard Denommée <moronicproduct3@gmail.com>
2019-02-07 20:24:18 -0500

    Chrome packed version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({executablePath:'/usr/bin/chromium'});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://securbot.gel.usherbrooke.ca/');
     await page.screenshot({path: 'example.png'});
