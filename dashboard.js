2d96c768d6655672511cff9c5fd4b13436f86f52
Édouard Denommée <dene2303@usherbrooke.ca>
2019-02-10 11:42:37 -0500

    Created dashboard funcgtion

---
+++ dashboard.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer-core');
+
+module.exports.getDashboard = async function (settings){
+    const browser = await puppeteer.launch({
+        executablePath: settings.chromium_path,
+        defaultViewport: {width: 1920, height: 1080}
+    });
+    const page = await browser.newPage();
+    console.log('Chromium started');
+
+    // Load Page
+    console.log('Loading page...');
+    await page.goto(settings.url);
+    console.log('Page loaded');
+
+    // Log into Jira
+    console.log('Filling login ...');
+    await page.type('#login-form-username', settings.user);
+    await page.type('#login-form-password', settings.passwd);
+    await page.click('#login');
+    console.log('Submitting login');
+
+    console.log('Waiting for dashboard ...');
+    await page.waitForNavigation({timeout: 120000});
+
+    console.log('Waiting for Worklogs ...');
+    var worklogFrame = page.frames().find(frame => frame.url().includes('worklog'));
+    await worklogFrame.waitForSelector('#worklogs_main>div>div.main-content', {timeout: 60000});
+
+    console.log('Acquiring dashboard')
+    var dashboard = await page.$('#dashboard-content');
+
+    console.log('Printing dashboard')
+    var image = await dashboard.screenshot({path: settings.output});
+    browser.close();
+
+    return image;
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer-core');
+const dModule = require('./dashboard.js');
 
 // Loading settings
 try{
@@ -8,37 +9,4 @@ try{
     process.exit(1);
 }
 
-(async () => {
-    const browser = await puppeteer.launch({
-        executablePath: settings.chromium_path,
-        defaultViewport: {width: 1920, height: 1080}
-    });
-    const page = await browser.newPage();
-    console.log('Chromium started');
-
-    // Load Page
-    console.log('Loading page...');
-    await page.goto(settings.url);
-    console.log('Page loaded');
-
-    // Log into Jira
-    console.log('Filling login ...');
-    await page.type('#login-form-username', settings.user);
-    await page.type('#login-form-password', settings.passwd);
-    await page.click('#login');
-    console.log('Submitting login');
-
-    console.log('Waiting for dashboard ...');
-    await page.waitForNavigation({timeout: 120000});
-
-    console.log('Waiting for Worklogs ...');
-    var worklogFrame = page.frames().find(frame => frame.url().includes('worklog'));
-    await worklogFrame.waitForSelector('#worklogs_main>div>div.main-content', {timeout: 60000});
-
-    console.log('Acquiring dashboard')
-    var dashboard = await page.$('#dashboard-content');
-
-    console.log('Printing dashboard')
-    await dashboard.screenshot({path: settings.output});
-    await browser.close();
-})();
+dModule.getDashboard(settings);
